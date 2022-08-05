e189b91c1e2a6fb625c89a16833727c803012686
histaladle <6569107@qq.com>
2022-08-05 14:39:44 +0800

    20220805-1

---
+++ test_bitop/MainWindow.cpp
@@ -0,0 +1,44 @@
+#include "MainWindow.h"
+#include "ui_MainWindow.h"
+#include <QDebug>
+void midbit(char *src,char *dst,int start_bit,int end_bit);
+void movebit(char *src,int src_bit_ofs,char *dst,int dst_bit_ofs,int bit_width);
+void swap_bit(char *data1,int bit1,char *data2,int bit2);
+void reverse_bit_order(char *data,int start_bit,int end_bit);
+void reverse_byte_order(char *data,int start_byte,int end_byte);
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QString s1,s2;
+    uchar src[8]={0x01,0x82,0x03,0x04,0x05,0x06,0x07,0x08};
+    uchar dst[8]={0};
+    midbit(reinterpret_cast<char*>(src),reinterpret_cast<char*>(dst),1,63);
+    s1.clear();
+    s2.clear();
+    for(int i=0;i<int(sizeof(src));i++) {
+        for(int j=0;j<8;j++) {
+            s1.append(src[i]&(1<<j)?"1":"0");
+            s2.append(dst[i]&(1<<j)?"1":"0");
+        }
+    }
+//    movebit(reinterpret_cast<char*>(src),8,reinterpret_cast<char*>(dst),0,16);
+//    s1.clear();
+//    s2.clear();
+//    for(int i=0;i<int(sizeof(src));i++) {
+//        for(int j=0;j<8;j++) {
+//            s1.append(src[i]&(1<<j)?"1":"0");
+//            s2.append(dst[i]&(1<<j)?"1":"0");
+//        }
+//    }
+    qDebug() << s1;
+    qDebug() << s2;
+    qDebug() << "debug10";
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
---
+++ test_bitop/test_bitop.pro
@@ -0,0 +1,21 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2022-07-06T10:29:37
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = test_bitop
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        MainWindow.cpp \
+    bitop.cpp
+
+HEADERS  += MainWindow.h
+
+FORMS    += MainWindow.ui
