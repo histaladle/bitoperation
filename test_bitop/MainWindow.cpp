57428f009b798266b748f099b8dd3d198031473a
histaladle <6569107@qq.com>
2023-02-11 10:14:25 +0800

    20230211-1

--- test_bitop/MainWindow.cpp
+++ test_bitop/MainWindow.cpp
@@ -6,6 +6,7 @@ void movebit(char *src,int src_bit_ofs,char *dst,int dst_bit_ofs,int bit_width);
 void swap_bit(char *data1,int bit1,char *data2,int bit2);
 void reverse_bit_order(char *data,int start_bit,int end_bit);
 void reverse_byte_order(char *data,int start_byte,int end_byte);
+void movebyte(char *data,int start_byte,int end_byte,int offset);
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -32,6 +33,20 @@ MainWindow::MainWindow(QWidget *parent) :
 //            s1.append(src[i]&(1<<j)?"1":"0");
 //            s2.append(dst[i]&(1<<j)?"1":"0");
 //        }
+//    }
+//    movebyte(reinterpret_cast<char*>(src),0,3,1);
+//    s1.clear();
+//    s2.clear();
+//    for(int i=0;i<int(sizeof(src));++i) {
+//        s1.append(QString::number(int(src[i])&0xff,16));
+//        s1.append(" ");
+//    }
+//    movebyte(reinterpret_cast<char*>(src),1,4,-1);
+//    s1.clear();
+//    s2.clear();
+//    for(int i=0;i<int(sizeof(src));++i) {
+//        s1.append(QString::number(int(src[i])&0xff,16));
+//        s1.append(" ");
 //    }
     qDebug() << s1;
     qDebug() << s2;
--- test_bitop/bitop.cpp
+++ test_bitop/bitop.cpp
@@ -1,15 +1,16 @@
 void midbit(char *src,char *dst,int start_bit,int end_bit)
 {
+    int start_ch=start_bit/8;
+    int end_ch=end_bit/8;
+    int ch_len=end_ch-start_ch+1;
+    int bit_ofs=start_bit%8;
+    char neg_mask;
+    int i=0;
 	if(start_bit>end_bit)
 	{
 		return;
 	}
-	int start_ch=start_bit/8;
-	int end_ch=end_bit/8;
-	int ch_len=end_ch-start_ch+1;
-	int bit_ofs=start_bit%8;
-	char neg_mask;
-	for(int i=0;i<ch_len;i++)
+    for(i=0;i<ch_len;++i)
 	{
 		if(i>0)
 		{
@@ -17,9 +18,13 @@ void midbit(char *src,char *dst,int start_bit,int end_bit)
 		}
 
 		dst[i]=src[i+start_ch]>>bit_ofs;
-		if(src[i+start_ch]<0&&bit_ofs>1)
+		if((src[i+start_ch]&0x80) && (bit_ofs>1))
 		{
-			neg_mask=-128;
+#ifdef __cplusplus
+            neg_mask=char(0x80);
+#else
+            neg_mask=(char)0x80;
+#endif
 			neg_mask>>=(bit_ofs-1);
 			dst[i]&=(~neg_mask);
 		}
@@ -28,12 +33,13 @@ void midbit(char *src,char *dst,int start_bit,int end_bit)
 
 void movebit(char *src,int src_bit_ofs,char *dst,int dst_bit_ofs,int bit_width)
 {
+    int s_ch_idx,d_ch_idx,s_bit_ofs,d_bit_ofs;
+    int i=0;
 	if(bit_width<0)
 	{
 		return;
 	}
-	int s_ch_idx,d_ch_idx,s_bit_ofs,d_bit_ofs;
-	for(int i=0;i<bit_width;i++)
+    for(i=0;i<bit_width;++i)
 	{
 		s_ch_idx=(src_bit_ofs+i)/8;
 		d_ch_idx=(dst_bit_ofs+i)/8;
@@ -71,13 +77,14 @@ void swap_bit(char *data1,int bit1,char *data2,int bit2)
 
 void reverse_bit_order(char *data,int start_bit,int end_bit)
 {
+    int bit_len=end_bit-start_bit+1;
+    int center_bit=(start_bit+end_bit)/2;
+    int i=0;
 	if(start_bit>end_bit)
 	{
 		return;
 	}
-	int bit_len=end_bit-start_bit+1;
-	int center_bit=(start_bit+end_bit)/2;
-	for(int i=0;i<bit_len/2;i++)
+    for(i=0;i<bit_len/2;++i)
 	{
 		swap_bit(data,center_bit-i,data,center_bit+i+(bit_len+1)%2);
 	}
@@ -85,17 +92,40 @@ void reverse_bit_order(char *data,int start_bit,int end_bit)
 
 void reverse_byte_order(char *data,int start_byte,int end_byte)
 {
+    int byte_len=end_byte-start_byte+1;
+    int center_byte=(start_byte+end_byte)/2;
+    char ch;
+    int i=0;
 	if(start_byte>end_byte)
 	{
 		return;
 	}
-	int byte_len=end_byte-start_byte+1;
-	int center_byte=(start_byte+end_byte)/2;
-	char ch;
-	for(int i=0;i<byte_len/2;i++)
+    for(i=0;i<byte_len/2;++i)
 	{
 		ch=data[center_byte-i];
 		data[center_byte-i]=data[center_byte+i+(byte_len+1)%2];
 		data[center_byte+i+(byte_len+1)%2]=ch;
 	}
+}
+
+void movebyte(char *data,int start_byte,int end_byte,int offset)
+{
+    int i=0;
+    if(start_byte>end_byte)
+    {
+        return;
+    }
+    if(offset>0)
+    {
+        for(i=end_byte;i>=start_byte;--i)
+        {
+            data[i+offset]=data[i];
+        }
+    }
+    else {
+        for(i=start_byte;i<=end_byte;++i)
+        {
+            data[i+offset]=data[i];
+        }
+    }
 }
